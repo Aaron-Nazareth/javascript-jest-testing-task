16c0effc5634bbb53a09529b370e0f726cadbbe9
Aaron Nazareth <aaronnazareth@hotmail.co.uk>
2022-03-03 14:15:51 +0000

    Add and subtract tests/methods complete

--- calculator_testing_start/calculator.test.js
+++ calculator_testing_start/calculator.test.js
@@ -16,21 +16,51 @@ describe('sum', () => {
     expect(actual).toBe(expected);
   });
 
-  test.skip('can add two large positive numbers', () => { 
-    
+  test('can add two large positive numbers', () => { 
+    expected = 10_000_000;
+    actual = sum(5_000_000, 5_000_000);
+    expect(actual).toBe(expected);
   });
 
-  test.skip('can add two negative numbers', () => {
-    
+  test('can add two negative numbers', () => {
+    expected = -10;
+    actual = sum(-4, -6);
+    expect(actual).toBe(expected);
   });
 
-  test.skip('can add zero', () => {
-    
+  test('can add zero', () => {
+    expected = 7;
+    actual = sum(0, 7);
+    expect(actual).toBe(expected);
   });
 
 });
 
 describe('subtract', () => {
+
+  test('can subtract two small positive numbers', () => {
+    expected = 5;
+    actual = subtract(10, 5);
+    expect(actual).toBe(expected);
+  });
+
+  test('can subtract two large positive numbers', () => { 
+    expected = 5_000_000;
+    actual = subtract(10_000_000, 5_000_000);
+    expect(actual).toBe(expected);
+  });
+
+  test('can subtract two negative numbers', () => {
+    expected = -10;
+    actual = subtract(-15, -5);
+    expect(actual).toBe(expected);
+  });
+
+  test('can subtract zero', () => {
+    expected = 7;
+    actual = subtract(7, 0);
+    expect(actual).toBe(expected);
+  });
 
 });
 
